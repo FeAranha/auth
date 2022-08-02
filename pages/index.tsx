0d43d5640669fd69f231db03a3745b43b1f82d18
fearanha <felipearanha.c@gmail.com>
2022-08-02 15:21:34 -0300

    setup

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from '../node_modules/next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <main>
+        <h1>hello wolrd</h1>
+      </main>
+    </div>
+  )
+}
